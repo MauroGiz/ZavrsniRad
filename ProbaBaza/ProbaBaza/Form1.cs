7ee0336209c81cb2f98eeb76b6e50acecb80104c
MauroGiz <35739412+MauroGiz@users.noreply.github.com>
2019-02-15 13:55:52 +0100

    Add files via upload

---
+++ ProbaBaza/ProbaBaza/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace ProbaBaza
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectionString;
+            SqlConnection cnn;
+            connectionString = @"Data Source=|DataDirectory|\Database1.mdf";
+                cnn = new SqlConnection(connectionString);
+            cnn.Open();
+            MessageBox.Show("Connection open!");
+            cnn.Close();
+        }
+    }
+}
